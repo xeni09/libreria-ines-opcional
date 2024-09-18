0585fc4eaad73237e33883434ab8bd105af98c79
inesrf <inesrf.art@gmail.com>
2024-09-18 11:06:02 +0200

    test

--- vars/sonarAnalysis.groovy
+++ vars/sonarAnalysis.groovy
@@ -1,8 +1,9 @@
 def call(Map config = [:]) {
     def abortPipeline = config.get('abortPipeline', false)
     def abortOnQualityGateFail = config.get('abortOnQualityGateFail', false)
+    def branchName = env.BRANCH_NAME
 
-    echo "Ejecución de las pruebas de calidad de código con SonarQube"
+    echo "Ejecución de las pruebas de calidad de código con SonarQube en la rama ${branchName}"
 
     withSonarQubeEnv('Sonar Local') {
         // Ejecutar el análisis con sonar-scanner (o un echo en su lugar)
@@ -14,7 +15,7 @@ def call(Map config = [:]) {
             def qg = [status: 'OK'] // Cambiar a 'ERROR' para simular fallo
             if (qg.status != 'OK') {
                 echo "Quality Gate status: ${qg.status}"
-                if (abortPipeline || abortOnQualityGateFail) {
+                if (abortPipeline || abortOnQualityGateFail || branchName == 'master' || branchName.startsWith('hotfix')) {
                     error "Abortando el pipeline debido a la falla en el Quality Gate"
                 } else {
                     echo "Continuando con el pipeline a pesar de la falla en el Quality Gate"
