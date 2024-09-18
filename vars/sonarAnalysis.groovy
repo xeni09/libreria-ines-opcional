401cf87e7a8dafe4baeb47205d9450bc8ae90230
inesrf <inesrf.art@gmail.com>
2024-09-18 10:49:25 +0200

    test

--- vars/sonarAnalysis.groovy
+++ vars/sonarAnalysis.groovy
@@ -1,29 +1,19 @@
 def call(Map config = [:]) {
     def abortPipeline = config.get('abortPipeline', false)
     def abortOnQualityGateFail = config.get('abortOnQualityGateFail', false)
-    def branchName = env.BRANCH_NAME
 
     echo "Ejecución de las pruebas de calidad de código con SonarQube"
 
     withSonarQubeEnv('Sonar Local') {
-        // Ejecutar el análisis con sonar-scanner
-        sh '''
-        export PATH=$PATH:/opt/homebrew/bin && sonar-scanner \
-          -Dsonar.projectKey=threepoints_devops_webserver_ines \
-          -Dsonar.projectName="DevOps Web Server Ines" \
-          -Dsonar.sources=./src \
-          -Dsonar.host.url=http://localhost:9000
-        '''
-
-        // Añadir un tiempo de espera para asegurar que el servidor procese el análisis
-        sh 'sleep 30'
+        // Ejecutar el análisis con sonar-scanner (o un echo en su lugar)
+        sh 'echo "Ejecución de las pruebas de calidad de código"'
 
         // Esperar el resultado del Quality Gate
-        timeout(time: 10, unit: 'MINUTES') {
+        timeout(time: 5, unit: 'MINUTES') {
             def qg = waitForQualityGate()
             if (qg.status != 'OK') {
                 echo "Quality Gate status: ${qg.status}"
-                if (abortPipeline || abortOnQualityGateFail || branchName == 'master' || branchName.startsWith('hotfix')) {
+                if (abortPipeline || abortOnQualityGateFail) {
                     error "Abortando el pipeline debido a la falla en el Quality Gate"
                 } else {
                     echo "Continuando con el pipeline a pesar de la falla en el Quality Gate"
