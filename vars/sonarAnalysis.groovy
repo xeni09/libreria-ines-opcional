86d6801a2ba614b83d6caa6c4794198c2692dd28
inesrf <inesrf.art@gmail.com>
2024-09-21 11:06:09 +0200

    nuevo token

--- vars/sonarAnalysis.groovy
+++ vars/sonarAnalysis.groovy
@@ -5,39 +5,41 @@ def call(Map config = [:]) {
 
     // Usar el entorno de SonarQube configurado en Jenkins
     withSonarQubeEnv('Sonar Local') {
-        try {
-            // Ejecutar el análisis real con SonarQube
-            echo "Ejecutando sonar-scanner..."
-            sh '''sonar-scanner \
-                -Dsonar.projectKey=threepoints_devops_webserver_ines \
-                -Dsonar.projectName="DevOps Web Server Ines" \
-                -Dsonar.sources=./src \
-                -Dsonar.host.url=http://localhost:9000 \
-                -Dsonar.login=${SONAR_TOKEN}'''
-            
-            // Añadir un pequeño delay antes de esperar el Quality Gate
-            echo "Durmiendo 5 segundos antes de esperar el Quality Gate..."
-            sleep(time: 5, unit: 'SECONDS')
+        withCredentials([string(credentialsId: 'NuevoToken', variable: 'SONAR_TOKEN')]) {
+            try {
+                // Ejecutar el análisis real con SonarQube
+                echo "Ejecutando sonar-scanner..."
+                sh '''sonar-scanner \
+                    -Dsonar.projectKey=threepoints_devops_webserver_ines \
+                    -Dsonar.projectName="DevOps Web Server Ines" \
+                    -Dsonar.sources=./src \
+                    -Dsonar.host.url=http://localhost:9000 \
+                    -Dsonar.login=${SONAR_TOKEN}'''
+                
+                // Añadir un pequeño delay antes de esperar el Quality Gate
+                echo "Durmiendo 5 segundos antes de esperar el Quality Gate..."
+                sleep(time: 5, unit: 'SECONDS')
 
-            // Esperar el resultado del Quality Gate con timeout de 5 minutos
-            timeout(time: 5, unit: 'MINUTES') {
-                def qualityGate = waitForQualityGate()
-                if (qualityGate.status != 'OK') {
-                    echo "Quality Gate status: ${qualityGate.status}"
+                // Esperar el resultado del Quality Gate con timeout de 5 minutos
+                timeout(time: 5, unit: 'MINUTES') {
+                    def qualityGate = waitForQualityGate()
+                    if (qualityGate.status != 'OK') {
+                        echo "Quality Gate status: ${qualityGate.status}"
 
-                    // Evaluar si debe abortar el pipeline según los parámetros booleanos
-                    if (abortPipeline || abortOnQualityGateFail) {
-                        error "Abortando el pipeline debido a la falla en el Quality Gate"
+                        // Evaluar si debe abortar el pipeline según los parámetros booleanos
+                        if (abortPipeline || abortOnQualityGateFail) {
+                            error "Abortando el pipeline debido a la falla en el Quality Gate"
+                        } else {
+                            echo "Continuando con el pipeline a pesar de la falla en el Quality Gate"
+                        }
                     } else {
-                        echo "Continuando con el pipeline a pesar de la falla en el Quality Gate"
+                        echo "Quality Gate aprobado"
                     }
-                } else {
-                    echo "Quality Gate aprobado"
                 }
+            } catch (Exception e) {
+                echo "Error durante la ejecución del análisis de SonarQube: ${e.getMessage()}"
+                error "Error en el análisis de SonarQube."
             }
-        } catch (Exception e) {
-            echo "Error durante la ejecución del análisis de SonarQube: ${e.getMessage()}"
-            error "Error en el análisis de SonarQube."
         }
     }
 }
